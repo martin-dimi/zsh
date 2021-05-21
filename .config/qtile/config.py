fb2c751db947c10851f7149f1e2362da77181032
TheWalkingFridge <martin.dimi97@gmail.com>
2021-05-21 12:38:33 +0100

    Update .config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,4 +1,5 @@
-import os, subprocess
+import os
+import subprocess
 from typing import List
 
 from libqtile import bar, layout, widget, hook
@@ -18,55 +19,60 @@ bring_front_click = False
 cursor_warp = False
 main = None  # WARNING: this is deprecated and will be removed soon
 
+
 def playpause(qtile):
-    qtile.cmd_spawn('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')
+    qtile.cmd_spawn(
+        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause')
+
 
 def nextSong(qtile):
-        qtile.cmd_spawn('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')
-    
+    qtile.cmd_spawn(
+        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next')
+
+
 def prevSong(qtile):
-        qtile.cmd_spawn('dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')
+    qtile.cmd_spawn(
+        'dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous')
+
 
 keys = [
+    # The essentials.
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "Return", lazy.spawn(
+        "dmenu_run -p 'RUN: '"), desc="Run launcher"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
     # Switch between windows
     Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    Key([mod, "shift"], "Return", lazy.spawn("dmenu_run -p 'RUN: '"), desc="Run launcher"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
 
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    # Key([mod, "shift"], "Return", lazy.layout.toggle_split(), desc="Toggle between split and unsplit sides of stack"),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-
-    # Audio related..
+    # Audio controls.
     Key([], 'XF86AudioLowerVolume', lazy.spawn('amixer -c 1 sset Master 1%-')),
     Key([], 'XF86AudioRaiseVolume', lazy.spawn('amixer -c 1 sset Master 1%+')),
     Key([], 'XF86AudioMute', lazy.spawn('amixer -c 1 sset Master 100%-')),
@@ -76,83 +82,170 @@ keys = [
     Key([], 'XF86AudioPrev', lazy.function(prevSong)),
 ]
 
+####################################################
+# Groups
+group_names = [
+    ("WWW", {'layout': 'monadtall'}),
+    ("DEV", {'layout': 'monadtall'}),
+    ("MUS", {'layout': 'monadtall'}),
+    ("MSC", {'layout': 'monadtall'}),
+]
 
-groups = [Group(i) for i in "123456789"]
+groups = [Group(name, **kwargs) for name, kwargs in group_names]
+for i, (name, kwargs) in enumerate(group_names, 1):
+    # Switch to another group
+    keys.append(Key([mod], str(i), lazy.group[name].toscreen()))
+    # Send current window to another group
+    keys.append(Key([mod, "shift"], str(i), lazy.window.togroup(name)))
 
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
-    ])
-
+####################################################
+# Layout
 layout_theme = {
-        "border_width": 2,
-        "margin": 4,
-        "border_focus": "e1acff",
-        "border_normal": "1D2330",
+    "border_width": 3,
+    "margin": 8,
+    "border_focus": "#82aaff",
+    "border_normal": "1D2330",
 }
 
 layouts = [
-    layout.Columns(**layout_theme),
+    layout.MonadTall(**layout_theme),
     layout.Max(**layout_theme),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Stack(num_stacks=2),
+    layout.Floating(**layout_theme),
 ]
 
+colors = [
+    ["#282c34", "#282c34"],  # panel background
+    ["#3d3f4b", "#434758"],  # background for current screen tab
+    ["#ffffff", "#ffffff"],  # font color for group names
+    ["#ff5555", "#ff5555"],  # border line color for current tab
+    # border line color for 'other tabs' and color for 'odd widgets'
+    ["#74438f", "#74438f"],
+    ["#4f76c7", "#4f76c7"],  # color for the 'even widgets'
+    ["#82aaff", "#82aaff"],  # window name
+    ["#82aaff", "#82aaff"],  # backbround for inactive screens
+]
+#prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
+
 widget_defaults = dict(
-    font='sans',
+    font="Ubuntu Mono",
     fontsize=12,
-    padding=3,
+    padding=2,
+    background=colors[0],
 )
 extension_defaults = widget_defaults.copy()
 
+default_widget_list = [
+    widget.CurrentLayout(),
+    widget.GroupBox(),
+    widget.Prompt(),
+    widget.WindowName(),
+    widget.Chord(
+        chords_colors={
+            'launch': ("#ff0000", "#ffffff"),
+        },
+        name_transform=lambda name: name.upper(),
+    ),
+    widget.TextBox("default config", name="default"),
+    widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
+    widget.Systray(),
+    widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+    widget.Volume(),
+    widget.QuickExit(),
+]
+
+widget_list = [
+    widget.Sep(
+        linewidth=0,
+        padding=6,
+    ),
+    widget.GroupBox(
+        font="Ubuntu Bold",
+        fontsize=9,
+        margin_y=3,
+        margin_x=0,
+        padding_y=5,
+        padding_x=3,
+        borderwidth=3,
+        active=colors[2],
+        inactive=colors[7],
+        rounded=False,
+        highlight_color=colors[1],
+        highlight_method="line",
+        this_current_screen_border=colors[6],
+        this_screen_border=colors[4],
+        other_current_screen_border=colors[6],
+        other_screen_border=colors[4],
+    ),
+    widget.Sep(
+        linewidth=0,
+        padding=15,
+    ),
+    widget.WindowName(
+        foreground=colors[6],
+    ),
+    widget.Systray(
+        padding=5,
+        background=colors[4],
+    ),
+    widget.Sep(
+        linewidth=0,
+        padding=6,
+    ),
+    widget.TextBox(
+        text=" 🖬",
+        padding=6,
+        fontsize=14,
+    ),
+    widget.Memory(
+        mouse_callbacks={
+            'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')},
+        padding=4,
+    ),
+    widget.Sep(
+        linewidth=2,
+        padding=6,
+    ),
+    widget.TextBox(
+        text=" Vol:",
+        padding=0
+    ),
+    widget.Volume(
+        padding=5
+    ),
+    widget.Sep(
+        linewidth=2,
+        padding=6,
+    ),
+    widget.CurrentLayout(
+        padding=5,
+    ),
+    widget.Sep(
+        linewidth=2,
+        padding=6,
+    ),
+    widget.Clock(
+        format="%A, %B %d - %H:%M ",
+        padding=5,
+    ),
+]
+
 screens = [
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        'launch': ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                widget.Systray(),
-                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
-                widget.Volume(),
-                widget.QuickExit(),
-            ],
+        top=bar.Bar(
+            widget_list,
             24,
         ),
     ),
 ]
 
+
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
@@ -166,12 +259,15 @@ floating_layout = layout.Floating(float_rules=[
     Match(title='branchdialog'),  # gitk
     Match(title='pinentry'),  # GPG key password entry
     Match(wm_class='spotify'),
+    Match(wm_class='android'),
 ])
+
 
 @hook.subscribe.startup
 def start_once():
     home = os.path.expanduser('~/.config/qtile/autostart.sh')
     subprocess.call([home])
+
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
--- .zshrc
+++ .zshrc
@@ -1,43 +1,79 @@
-# If you come from bash you might have to change your $PATH.
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-
-# Path to your oh-my-zsh installation.
-export DOOM="/home/mdimitrov/.emacs.d/bin"
-export GOPATH="/home/mdimitrov/code:/home/mdimitrov/code/src/plz-out/gen/third_party/go:/home/mdimitrov/code/src/plz-out/gen/third_party/go/kubernetes:/home/mdimitrov/code/src/plz-out/gen/third_party/go/operator-sdkcore3"
-export ZSH="/home/mdimitrov/.oh-my-zsh"
-export EDITOR='vim'
-
-# Theme
-ZSH_THEME="powerlevel10k/powerlevel10k"
+# Enable Powerlevel10k instant prompt. Put this at the top. Enables plugins to load
+# in the background..
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
-# Uncomment the following line to disable bi-weekly auto-update checks.
-DISABLE_AUTO_UPDATE="true"
+###################################################
+# Options
+setopt correct                                                  # Auto correct mistakes
+setopt extendedglob                                             # Regular expressions with * (globbing)
+setopt nocaseglob                                               # Case insensitive globbing
+setopt nocheckjobs                                              # Don't warn about running processes when exiting
+setopt numericglobsort                                          # Sort filenames numerically when it makes sense
+setopt nobeep                                                   # No beep
+setopt appendhistory                                            # Append history instead of overwriting
+setopt histignorealldups                                        # Don't duplicate history commands
+setopt autocd                                                   # If only directory path is entered, cd there.
 
-# Uncomment the following line to automatically update without prompting.
-DISABLE_UPDATE_PROMPT="true"
+HISTFILE=~/.cache/zsh/zhistory
+HISTSIZE=10000
+SAVEHIST=10000
+setopt SHARE_HISTORY
+export EDITOR="emacsclient -nw"
+export VISUAL="emacsclient -nw"
+export TERM=xterm-24bit
 
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS="true"
+# Auto Completion
+autoload -U compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'       # Case insensitive tab completion
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"         # Colored completion (different colors for dirs/files/etc)
+zstyle ':completion:*' rehash true                              # automatically find new executables in path
+# Speed up completions
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)
 
-plugins=(
-  git
-  zsh-autosuggestions
-  zsh-syntax-highlighting # Always put this as the last plugin to load.
-)
-source $ZSH/oh-my-zsh.sh
+# Edit line in vim with ctrl-e. 
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^e' edit-command-line
 
-# Handle kubernetes.
-source <(kubectl completion zsh)
-complete -F __start_kubectl kubectl
-export KUBECONFIG=/home/mdimitrov/.kube/config
-for file in /home/mdimitrov/.kube/configs/*.yaml; do
-  export KUBECONFIG=$KUBECONFIG:$file
-done
+###################################################
+# Key bindings
+bindkey -v							                                        # Vim mode
+bindkey '^[[7~' beginning-of-line                               # Home key
+bindkey '^[[H' beginning-of-line                                # Home key
+bindkey '^[[8~' end-of-line                                     # End key
+bindkey '^[[F' end-of-line                                      # End key
+bindkey '^[[C'  forward-char                                    # Right key
+bindkey '^[[D'  backward-char                                   # Left key
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-if [ -e ~/.profile_tm ]; then source ~/.profile_tm; fi
-if [ -e ~/.bash_aliases ]; then  source ~/.bash_aliases; fi
+# Navigate words with ctrl+arrow keys
+bindkey '^[Oc' forward-word                                     #
+bindkey '^[Od' backward-word                                    #
+bindkey '^[[1;5D' backward-word                                 #
+bindkey '^[[1;5C' forward-word                                  #
+bindkey '^H' backward-kill-word                                 # delete previous word with ctrl+backspace
+bindkey '^[[Z' undo                                             # Shift+tab undo last action
+
+###################################################
+# Aliases
+alias vim="nvim"
+alias e="emacsclient -nw"
+alias zshrc="nvim ~/.config/zsh/.zshrc"
+alias src="source ~/.config/zsh/.zshrc"
+
+alias cat="bat"
+alias ls='lsd'
+alias ll="ls -al"
+
+###################################################
+# Plugins
+source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >/dev/null 
+source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+source ~/.config/zsh/p10k.zsh 					# Theme. p10k configure to edit.
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
