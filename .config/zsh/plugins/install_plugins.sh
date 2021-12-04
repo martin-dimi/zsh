667e5f823308b7ab8152aa5d9804e49f7f0ebdcf
Martin Dimitrov <martin@Martins-MacBook-Pro.local>
2021-12-04 18:24:56 +0000

    Updated scripts

---
+++ .config/zsh/plugins/install_plugins.sh
@@ -0,0 +1,13 @@
+PLUGIN_DIR=$HOME/.config/zsh/plugins
+
+echo "Installing xterm-24bit colors"
+/usr/bin/tic -x -o ~/.terminfo ./xterm-24bit.terminfo
+
+echo "Installing plugins"
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+git clone https://github.com/zsh-users/zsh-autosuggestions $PLUGIN_DIR/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $PLUGIN_DIR/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $PLUGIN_DIR/powerlevel10k
+
+echo "Done"
+source $HOME/.config/zsh/.zshrc
