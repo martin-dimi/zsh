a446cbfb028acaca8bff7edc29ac80c14202435e
martin-dimi <martin.dimi97@gmail.com>
2021-12-04 23:11:53 +0000

    updated shortcuts to use emacs

--- .config/zsh/zsh_aliases.zsh
+++ .config/zsh/zsh_aliases.zsh
@@ -6,8 +6,8 @@ alias ew="emacsclient -c -a 'emacs'"
 alias e="emacsclient -nw"
 
 # Quick access.
-alias zshrc="nvim ~/.config/zsh/.zshrc"
-alias aliases="nvim ~/.config/zsh/zsh_aliases.zsh"
+alias zshrc="e ~/.config/zsh/.zshrc"
+alias aliases="e ~/.config/zsh/zsh_aliases.zsh"
 alias src="source ~/.config/zsh/.zshrc"
 
 alias cat="bat"
